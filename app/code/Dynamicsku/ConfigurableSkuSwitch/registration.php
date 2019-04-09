a8817a3f94334b7d941c5f796a4b1991edf38845
ramulup <ramulup@kensium.com>
2019-04-09 16:51:57 +0530

    deleted app folder

--- app/code/Dynamicsku/ConfigurableSkuSwitch/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Dynamicsku_ConfigurableSkuSwitch',
-    __DIR__
-);
