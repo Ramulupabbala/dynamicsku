c77e71f6af8eab60722596620e218c8c2e1538e6
ramulup <ramulup@kensium.com>
2019-05-20 17:57:27 +0530

    add module

---
+++ Cate/Attr/Model/Config/Source/Options.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Cate\Attr\Model\Config\Source;
+/**
+ * Catalog category landing page attribute source
+ *
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Options extends \Magento\Eav\Model\Entity\Attribute\Source\AbstractSource
+{
+    /**
+     * {@inheritdoc}
+     * @codeCoverageIgnore
+     */
+    public function getAllOptions()
+    {
+        if (!$this->_options) {
+            $this->_options = [
+                ['value' => 'l1', 'label' => __('L1')],
+                ['value' => 'l2', 'label' => __('L2')],
+                ['value' => 'l3', 'label' => __('L3')],
+            ];
+        }
+        return $this->_options;
+    }
+      /**
+     * Get options in "key-value" format
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return [
+            'l1' => __('L1'),
+            'l2' => __('L2'),
+            'L3' => __('L3'),
+            ];
+    }
+
+}
---
+++ Cate/Attr/Setup/InstallData.php
@@ -0,0 +1,51 @@
+<?php
+namespace Cate\Attr\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Catalog\Model\Category;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class InstallData implements InstallDataInterface
+{
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     *
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            Category::ENTITY,
+            'category_url',
+            [
+                'type' => 'int',
+                'label' => 'Category Url',
+                'input' => 'select',
+                'source' => 'Cate\Attr\Model\Config\Source\Options',
+                'required' => false,
+                'sort_order' => 200,
+                'global' => ScopedAttributeInterface::SCOPE_STORE,
+                'group' => 'General Information',
+            ]
+        );
+    }
+}
+
